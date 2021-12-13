29748a84f9272a38daeb9ec4740f27460abeb941
Irina <irina.s.tomilova@yandex.ru>
2021-12-13 16:53:22 +0500

    Добавили маршрутизацию и систему MVC

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,8 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddControllersWithViews();
+
 var app = builder.Build();
+
+//для перехвата исключений в браузере
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
 
 var config = app.Configuration;
 
-app.MapGet("/", () => config["CustomGreetings"]);
+//app.MapGet("/", () => config["CustomGreetings"]);
+app.MapGet("/throw", () =>
+{
+    throw new ApplicationException("Ошибка в программе!");
+});
+
+app.MapDefaultControllerRoute();
 
 app.Run();
