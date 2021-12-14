3c1126b8306f2d548be2218c36b5b50091a3441f
Irina <irina.s.tomilova@yandex.ru>
2021-12-14 14:51:32 +0500

    Страничка списка сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 25 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 29 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 23 }
+        };
+
         public IActionResult Index()
         {
             //return Content("Данные из первого контроллера");
@@ -13,6 +21,11 @@ namespace WebStore.Controllers
         public string ConfiguredAction(string id, string value)
         {
             return $"Hello World! {id} - {value}";
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }
