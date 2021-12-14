5c719a51574dde756c117a30b123e7ea8131e421
Irina <irina.s.tomilova@yandex.ru>
2021-12-14 14:31:46 +0500

    Дополнительные параметры в методах контроллера

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,5 +9,10 @@ namespace WebStore.Controllers
             //return Content("Данные из первого контроллера");
             return View();
         }
+
+        public string ConfiguredAction(string id, string value)
+        {
+            return $"Hello World! {id} - {value}";
+        }
     }
 }
