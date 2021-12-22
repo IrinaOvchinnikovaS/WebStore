91c55aad6908c7b72f0cfa9881f4aea48b04c2ff
Irina <irina.s.tomilova@yandex.ru>
2021-12-22 22:29:56 +0500

    Выселение сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebStore.Models;
+using WebStore.Data;
 
 namespace WebStore.Controllers
 {
@@ -7,13 +8,12 @@ namespace WebStore.Controllers
     //[Route("Staff/{action=Index}/{id?}")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 25, Education = "ПГНИУ Мехмат", WorkExperience = "Прогноз, 2019-2021" },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 29, Education = "ПНИПУ электротех", WorkExperience = "Xolla, 2020-н.в." },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 23, Education = "ПГНИУ физфак", WorkExperience = "Завод Шпагина, 2020-н.в." }
-        };
+        private ICollection<Employee> _employees;
 
+        public EmployeesController()
+        {
+            _employees = TestData.Employees;
+        }
         public IActionResult Index()
         {
             return View(_employees);
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,14 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public static class TestData
+    {
+        public static List<Employee> Employees { get; } = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 25, Education = "ПГНИУ Мехмат", WorkExperience = "Прогноз, 2019-2021" },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 29, Education = "ПНИПУ электротех", WorkExperience = "Xolla, 2020-н.в." },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 23, Education = "ПГНИУ физфак", WorkExperience = "Завод Шпагина, 2020-н.в." }
+        };
+    }
+}
