776b9f4adaefdc98a8c13954cc3da24a6130db44
Irina <irina.s.tomilova@yandex.ru>
2021-12-25 23:01:06 +0500

    Базовые абстрактные классы сущностей

--- WebStore.Domain/Entities/Base/Interfaces/INamedEntity.cs
+++ WebStore.Domain/Entities/Base/Interfaces/INamedEntity.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebStore.Domain.Entities.Base.Interfaces
+﻿namespace WebStore.Domain.Entities.Base.Interfaces
 {
     public interface INamedEntity : IEntity
     {
-        string name { get; set; }
+        string Name { get; set; }
     }
 }
