a8cba2e7aed3d48ca8a3970513aebff46b89463c
Irina <irina.s.tomilova@yandex.ru>
2021-12-25 22:18:13 +0500

    Примеры работы с коллекцией сервисов

---
+++ Tests/TestConsole/Services/WriteToFileProcessor.cs
@@ -0,0 +1,17 @@
+﻿using TestConsole.Data;
+using TestConsole.Services.Interfaces;
+
+namespace TestConsole.Services
+{
+    public class WriteToFileProcessor : IDataProcessor
+    {
+        public static string DataFilename { get; set; } = "data.txt";
+
+        public void Process(DataValue Value)
+        {
+            var str = string.Format("[{0}]({1}):{2}", Value.Id, Value.Time, Value.Value);
+            using var writer = File.AppendText(DataFilename);
+            writer.WriteLine(str);
+        }
+    }
+}
