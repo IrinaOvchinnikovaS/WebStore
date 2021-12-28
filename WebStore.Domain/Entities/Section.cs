b9ea5a8607dc414b244e99539710d210482fda82
Irina <irina.s.tomilova@yandex.ru>
2021-12-28 14:29:04 +0500

    Подготовка модели данных для размещения её в БД

---
+++ WebStore.Domain/Entities/Section.cs
@@ -0,0 +1,19 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebStore.Domain.Entities;
+
+[Index(nameof(Name), IsUnique = true)]
+public class Section : NamedEntity, IOrderedEntity
+{
+    public int Order { get; set; }
+
+    public int? ParentId { get; set; }
+
+    [ForeignKey(nameof(ParentId))]
+    public Section Parent { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+}
