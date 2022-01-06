3ba54763aeea483f515c1727ce2a9a5212c5215e
Irina <irina.s.tomilova@yandex.ru>
2022-01-06 23:35:45 +0500

    Удаление костылей

--- WebStore/Services/DbInitializer.cs
+++ WebStore/Services/DbInitializer.cs
@@ -116,6 +116,8 @@ namespace WebStore.Services
             _Logger.LogInformation("Инициализация сотрудников...");
             await using var transaction = await _db.Database.BeginTransactionAsync(Cancel);
 
+            TestData.Employees.ForEach(employee => employee.Id = 0);
+
             await _db.Employees.AddRangeAsync(TestData.Employees, Cancel);
             await _db.SaveChangesAsync(Cancel);
 
