0a7108b55709818d58824f7c4728c09bb45dcfbb
Irina <irina.s.tomilova@yandex.ru>
2022-01-16 20:10:57 +0500

    Инициализация данных системы Identity в БД

--- WebStore.Domain/Entities/Identity/Role.cs
+++ WebStore.Domain/Entities/Identity/Role.cs
@@ -5,5 +5,8 @@ namespace WebStore.Domain.Entities.Identity
 {
     public class Role : IdentityRole
     {
+        public const string Administrators = "Administrators";
+
+        public const string Users = "Users";
     }
 }
